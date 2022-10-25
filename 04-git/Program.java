3556059640b3fa2032e27bbf9a9668c0ef534e9a
unknown <student@lab-44-6.cs.put.poznan.pl>
2022-10-25 12:34:47 +0200

    Program.java final

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
 
 // komentarz
 
-=======
->>>>>>> 5b483b5 (zmiana Program.java)
 public class New {
-  int x = 5;
+  int x = 15;
 
   public static void main(String[] args) {
-<<<<<<< HEAD
     New myObj1 = new New();
     System.out.println(myObj1.x);
-=======
-    New myObj = new New();
-    System.out.println(myObj.x);
->>>>>>> 5b483b5 (zmiana Program.java)
   }
 }
